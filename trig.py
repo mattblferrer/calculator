e1ae2c97ecdb3dd7f3a44b3cb5c15a94a0ad2a7c
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2022-07-31 17:17:13 +0800

    added hyperbolic trig functions

--- trig.py
+++ trig.py
@@ -1,4 +1,5 @@
-from math import sin, cos, tan, asin, acos, atan, pi
+from math import sin, cos, tan, asin, acos, atan, sinh, cosh, tanh, asinh,\
+    acosh, atanh, pi
 
 
 # one number input
@@ -29,14 +30,33 @@ def rad_to_deg(a):
 def user_interface():
     while True:
         # menu of functions
-        print("1. Sine")
-        print("2. Cosine")
-        print("3. Tangent")
-        print("4. Inverse Sine")
-        print("5. Inverse Cosine")
-        print("6. Inverse Tangent")
-        print("7. Convert Degrees to Radians")
-        print("8. Convert Radians to Degrees")
+        print("\nConversion")
+        print("1. Convert Degrees to Radians")
+        print("2. Convert Radians to Degrees")
+
+        print("\nNormal functions")
+        print("3. Sine")
+        print("4. Cosine")
+        print("5. Tangent")
+        print("6. Secant")
+        print("7. Cosecant")
+        print("8. Cotangent")
+
+        print("\nHyperbolic Functions")
+        print("9. Hyperbolic Sine")
+        print("10. Hyperbolic Cosine")
+        print("11. Hyperbolic Tangent")
+        print("12. Hyperbolic Secant")
+        print("13. Hyperbolic Cosecant")
+        print("14. Hyperbolic Cotangent")
+
+        print("\nInverse Functions")
+        print("15. Inverse Sine")
+        print("16. Inverse Cosine")
+        print("17. Inverse Tangent")
+        print("18. Inverse Hyperbolic Sine")
+        print("19. Inverse Hyperbolic Cosine")
+        print("20. Inverse Hyperbolic Tangent")
 
         # user selection
         selection = input("Select function. ")
@@ -46,38 +66,72 @@ def user_interface():
             a = one_num_input()
 
             # evaluate selection
-            if int(selection) in list(range(1, 7)):
-                if selection == "1":  # sine
-                    radians = sin(a)
-                elif selection == "2":  # cosine
-                    radians = cos(a)
-                elif selection == "3":  # tangent
-                    radians = tan(a)
-                elif selection == "4":  # inverse sine
+            if int(selection) in list(range(1, 3)):
+                if selection == "1":  # degrees to radians
+                    answer = deg_to_rad(a)
+
+                    # print answer
+                    print("Answer: {} radians".format(answer))
+
+                elif selection == "2":  # radians to degrees
+                    answer = rad_to_deg(a)
+
+                    # print answer
+                    print("Answer: {} degrees".format(answer))
+
+            elif int(selection) in list(range(3, 9)):
+                if selection == "3":  # sine
+                    answer = sin(a)
+                elif selection == "4":  # cosine
+                    answer = cos(a)
+                elif selection == "5":  # tangent
+                    answer = tan(a)
+                elif selection == "6":  # secant
+                    answer = 1/cos(a)
+                elif selection == "7":  # cosecant
+                    answer = 1/sin(a)
+                elif selection == "8":  # cotangent
+                    answer = 1/tan(a)
+
+                # print answer
+                print("Answer: {}".format(answer))
+
+            elif int(selection) in list(range(9, 15)):
+                if selection == "9":  # hyp sine
+                    answer = sinh(a)
+                elif selection == "10":  # hyp cosine
+                    answer = cosh(a)
+                elif selection == "11":  # hyp tangent
+                    answer = tanh(a)
+                elif selection == "12":  # hyp secant
+                    answer = 1/cosh(a)
+                elif selection == "13":  # hyp cosecant
+                    answer = 1/sinh(a)
+                elif selection == "14":  # hyp cotangent
+                    answer = 1/tanh(a)
+
+                # print answer
+                print("Answer: {}".format(answer))
+
+            elif int(selection) in list(range(15, 21)):
+                if selection == "15":  # inverse sine
                     radians = asin(a)
-                elif selection == "5":  # inverse cosine
+                elif selection == "16":  # inverse cosine
                     radians = acos(a)
-                elif selection == "6":  # inverse tangent
-                    radians = atan(a)   
+                elif selection == "17":  # inverse tangent
+                    radians = atan(a)
+                elif selection == "18":  # inverse hyp sine
+                    radians = asinh(a)
+                elif selection == "19":  # inverse hyp cosine
+                    radians = acosh(a)
+                elif selection == "20":  # inverse hyp tangent
+                    radians = atanh(a) 
                 
                 # convert to degrees
                 degrees = radians / pi * 180
 
                 # print in rad and deg
                 print("Answer: {} ({} degrees)".format(radians, degrees)) 
-
-            elif int(selection) in list(range(7, 9)):
-                if selection == "7":  # degrees to radians
-                    answer = deg_to_rad(a)
-
-                    # print answer
-                    print("Answer: {} radians".format(answer))
-
-                elif selection == "8":  # radians to degrees
-                    answer = rad_to_deg(a)
-
-                    # print answer
-                    print("Answer: {} degrees".format(answer))
 
             else:
                 print("Invalid selection.\n")
